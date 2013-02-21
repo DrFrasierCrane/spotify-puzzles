b13e05f2ffe8af6780310389284cf59868cae1e4
DrFrasierCrane <tylersartin@gmail.com>
2013-02-20 20:53:31 -0500

    Still not working correctly. Code is highly refined and minimal. I think the problem is a discrepancy between Python 2.6 and 2.7 with respect to floats.

--- zipfsong.py
+++ zipfsong.py
@@ -1,73 +1,43 @@
-import collections
+## Code written by Tyler Sartin 20 February 2013 ##
+from operator import itemgetter
 
-## Take initial input of n (number of lines) and m (number of songs for output).
-nm = raw_input().split()
+[n, m] = map(int, raw_input().split())
 
-## After splitting the input, convert them to integers.
-[n, m] = map(int, nm)
+frequencies = []
+track_names = []
 
-## Create new (ordered) dictionary and for all n add to dictionary the name of track: and number
-## of plays (fi). The number of plays is changed to integer.
-songs = collections.OrderedDict()
-
-## For all tracks, split titles and frequencies. Then add them to the dictionary by putting
-## track title first, then frequency.
+## For all tracks, split titles and frequencies.
 for i in range(n):
 	item = raw_input().split()
-	songs[item[1]] = int(item[0])
+	frequencies.append(item[0])
+	track_names.append(item[1])
 
-#print songs
+
+frequencies = map(int, frequencies)
+
 ## Create three lists, fis is a list of the frequencies, zis is list of the Zipf values
 ## and qis is a list of qualities.
-## zi = (n - i) / n
+## zi = 1 / 1 + i
 ## qi = fi / zi
 
-fis = []
-for item in songs:
-	fis.append(songs[item])
-
-
-zis = []
-qis = []
+zipfs = []
+qualities = []
 for i in range(n):
-	zis.append(1 / (1.0 + i))
-	qis.append(fis[i] / zis[i])
+	zipfs.append(1 / (1.0 + i))
 
 
-## Replace the frequencies in the songs list with the qualities, qi.
-k = 0
-for item in songs:
-	songs[item] = qis[k]
-	k = k + 1
-
-## Sort and reverse the quality list into descending order.
-qis = list(set(qis))
-qis.sort()
-qis.reverse()
-
-##print fis
-##print zis
-##print qis
-##print songs
+for i in range(n):
+	qualities.append(frequencies[i] / zipfs[i])
 
 
-## This function takes in the output number required, the quality list, and the songs
-## list and proceeds to create the output list.
-def check(qis, songs):
-	for i in range(len(qis)):
-		for item in songs:
-			if qis[i] == songs[item]:
-				output.append(item)
-				
+trk_qual = {}
+
+for i in range(n):
+	trk_qual[track_names[i]] = qualities[i]
 
 
-global output
-output = []
+output = sorted(trk_qual.iteritems(), key=itemgetter(1))
+output.reverse()
 
-check(qis, songs)
-
-## Print the outputs.
 for i in range(m):
-	print output[i]
-## output1 = list(set(output))	
-## print output
+	print output[i][0]
